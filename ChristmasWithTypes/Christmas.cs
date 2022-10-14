6674d58a48c62e4b9111b298d77b297844b495e6
Mike Cowell <spike@spikeandbetty.com>
2022-10-13 20:34:16 -0400

    initial commit

---
+++ ChristmasWithTypes/Christmas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ChristmasWithTypes
+{
+    public class Christmas
+    {
+        public string[] Presents;
+        public string Santa { get; set; }
+        public int? TreeHeight = null;//{ get; set; } TODO Make the Height property nullable
+
+        //TODO Make the property, "Day", type enum
+        public enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat,
+            Thursday
+        }
+    }
+}
